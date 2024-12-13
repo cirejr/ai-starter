0c2affda39393aa1163ae6bae367f60a9041b9ab
Cire Jr <juniorcireba@gmail.com>
2024-12-13 14:23:09 +0000

    ft: basic multi-modal

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -12,37 +12,7 @@ export async function POST(req: Request) {
 
   const result = streamText({
     model: google("gemini-1.5-flash"),
-    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
-    Always run the action "getInformation" first to ensure you have the most up to date information.
-    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
     messages,
-    tools: {
-      addResource: tool({
-        description: `add a resource to your knowledge base.
-          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
-        parameters: z.object({
-          content: z
-            .string()
-            .describe("the content or resource to add to the knowledge base"),
-        }),
-        execute: async ({ content }) => createResource({ content }),
-      }),
-      getInformation: tool({
-        description: `get information from your knowledge base to answer questions.`,
-        parameters: z.object({
-          question: z.string().describe("the users question"),
-        }),
-        execute: async ({ question }) => {
-          console.log("Executing getInformation for question:", question);
-          try {
-            const results = await findRelevantContent(question);
-            return results.map((r) => r.name).join("\n");
-          } catch (e) {
-            return "Sorry, I couldn't find any relevant information.";
-          }
-        },
-      }),
-    },
   });
 
   return result.toDataStreamResponse();
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,70 @@
 "use client";
 
 import { useChat } from "ai/react";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    maxSteps: 3,
-  });
+  const { messages, input, handleInputChange, handleSubmit, isLoading } =
+    useChat();
+
+  const [files, setFiles] = useState<FileList | undefined>(undefined);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="stretch mx-auto flex w-full max-w-md flex-col py-24">
       <div className="space-y-4">
         {messages.map((m) => (
           <div key={m.id} className="whitespace-pre-wrap">
+            {m.role === "user" ? "User: " : "AI: "}
+            {m.content}
             <div>
-              <div className="font-bold">{m.role}</div>
-              <p>
-                {m.content.length > 0 ? (
-                  m.content
-                ) : (
-                  <span className="font-light italic">
-                    {"calling tool: " + m?.toolInvocations?.[0].toolName}
-                  </span>
-                )}
-              </p>
+              {m?.experimental_attachments
+                ?.filter((attachment) =>
+                  attachment?.contentType?.startsWith("image/")
+                )
+                .map((attachment, index) => (
+                  <Image
+                    key={`${m.id}-${index}`}
+                    src={attachment.url}
+                    width={500}
+                    height={500}
+                    alt={attachment.name ?? `attachment-${index}`}
+                  />
+                ))}
             </div>
           </div>
         ))}
       </div>
 
-      <form onSubmit={handleSubmit}>
+      <form
+        className="fixed bottom-0 mb-8 w-full max-w-md space-y-2 rounded border border-gray-300 p-2 shadow-xl"
+        onSubmit={(event) => {
+          handleSubmit(event, {
+            experimental_attachments: files,
+          });
+
+          setFiles(undefined);
+
+          if (fileInputRef.current) {
+            fileInputRef.current.value = "";
+          }
+        }}
+      >
         <input
-          className="fixed bottom-0 mb-8 w-full max-w-md rounded border border-gray-300 p-2 shadow-xl"
+          type="file"
+          className=""
+          onChange={(event) => {
+            if (event.target.files) {
+              setFiles(event.target.files);
+            }
+          }}
+          multiple
+          ref={fileInputRef}
+        />
+        <input
+          disabled={isLoading}
+          className="w-full p-2"
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
